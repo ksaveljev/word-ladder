715ef3e7ee435367bb5f89786017222d1f80cf0b
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-01-07 12:35:58 +0200

    Initial working version of Fatvat Word Ladder. Fast 'first' but very very slow 'quickest' version

--- src/WordLadderFatvat.hs
+++ src/WordLadderFatvat.hs
@@ -1,4 +1,7 @@
+import System.Environment (getArgs)
 import Data.Char (toLower, isAlpha)
+import Data.List (minimumBy)
+import Data.Ord (comparing)
 import qualified Data.Set as S
 
 type WordSet = S.Set String
@@ -27,5 +30,35 @@ buildGraph wordset top = Node top (map (buildGraph smaller) neighbours)
   where neighbours = S.toList (S.filter (neighbour top) smaller)
         smaller = S.delete top wordset
 
+search :: Node -> Int -> String -> [String]
+search graph maxDepth goal = search' graph maxDepth goal []
+
+search' :: Node -> Int -> String -> [String] -> [String]
+search' (Node end children) maxDepth goal path
+  | end == goal = end : path
+  | null children = []
+  | length path >= maxDepth = [] -- too deep
+  | difference end goal >= maxDepth - length path = [] -- too much difference
+  | otherwise = first
+    where
+      childRoutes = filter (not . null) $ map (\child -> search' child maxDepth goal (end : path)) children
+      first | null childRoutes = []
+            | otherwise = head childRoutes
+      quickest | null childRoutes = []
+               | otherwise = minimumBy (comparing length) childRoutes
+
+makeLadder :: Int -> String -> String -> IO [String]
+makeLadder maxDepth start end
+  | length start /= length end = error "Only two string of equal length are currently supported."
+  | otherwise = do
+      dict <- createDictionary (length start)
+      return $
+        if S.member start dict && S.member end dict
+          then search (buildGraph dict start) maxDepth end
+          else []
+
 main :: IO ()
-main = undefined
+main = do
+    args <- getArgs
+    ladder <- makeLadder 100 (head args) (args !! 1)
+    print ladder
