90e6600268a1ba9e99e20e4b249bc8f3bca0b112
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-01-10 18:10:35 +0200

    Trie data definition

--- src/WordLadderDarkOtter.hs
+++ src/WordLadderDarkOtter.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE BangPatterns #-}
+
 import Prelude hiding (foldl, foldr, foldl1, foldr1, (.), id)
+
+data Trie a = Trie !Bool [Node a] deriving (Show, Eq)
+data Node a = Node ! a   (Trie a) deriving (Show, Eq)
 
 main :: IO ()
 main = undefined
